3d2eae9f6c457637c8040f679b47752f979f6235
Martin Gaitan <mgaitan@onapsis.com>
2017-05-13 10:43:16 -0300

    first attempt

---
+++ build-wheels.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e -x
+
+# Install a system package required by our library
+yum install libsndfile-devel libsamplerate-devel
+yum install ffmpeg
+rpm -Uvh https://repos.jethrocarr.com/pub/amberdms/linux/centos/5/amberdms-os/"$ARCH"/RPMS/espeak-1.45.05-4.el5."$ARCH".rpm
+rpm -Uvh https://repos.jethrocarr.com/pub/amberdms/linux/centos/5/amberdms-os/"$ARCH"/RPMS/espeak-devel-1.45.05-4.el5."$ARCH".rpm
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install -r /io/dev-requirements.txt
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" -w /io/wheelhouse/
+done
+
+# Install packages and test
+# for PYBIN in /opt/python/*/bin/; do
+#     "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
+#     (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
+# done
