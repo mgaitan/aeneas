760c54ef5bd401ca866de17cac2ff5cea97b8962
Martin Gaitan <mgaitan@onapsis.com>
2017-05-13 14:21:58 -0300

    missing portaudio

--- build-wheels.sh
+++ build-wheels.sh
@@ -2,8 +2,7 @@
 set -e -x
 
 # Install a system package required by our library
-yum install -y libsndfile-devel libsamplerate-devel
-yum install portaudio
+yum install -y libsndfile-devel libsamplerate-devel portaudio
 # yum install -y ffmpeg
 
 # from http://wiki.neuralbs.com/index.php/Installing_Espeak_on_AsteriskNow
